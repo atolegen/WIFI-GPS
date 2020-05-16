2fb825e05f71f9efd8955b3bce60c02b5d9f0e75
atolegen <58818696+atolegen@users.noreply.github.com>
2020-05-16 11:11:51 +0600

    g

---
+++ App13/MainPage.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+
+
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Services.Maps;
+using System.Text;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App13
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        StringBuilder sb = new StringBuilder();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void Seattle_Click(object sender, RoutedEventArgs e)
+        {
+            Geopoint seattlePoint = new Geopoint
+                (new BasicGeoposition { Latitude = 47.6062, Longitude = -122.3321 });
+
+            PlaceInfo spaceNeedlePlace = PlaceInfo.Create(seattlePoint);
+
+            FrameworkElement targetElement = (FrameworkElement)sender;
+
+            GeneralTransform generalTransform =
+                targetElement.TransformToVisual((FrameworkElement)targetElement.Parent);
+
+            Rect rectangle = generalTransform.TransformBounds(new Rect(new Point
+                (targetElement.Margin.Left, targetElement.Margin.Top), targetElement.RenderSize));
+
+            spaceNeedlePlace.Show(rectangle, Windows.UI.Popups.Placement.Below);
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Specify a known location.
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+
+            // Set the map location.
+            MapControl1.Center = cityCenter;
+            MapControl1.ZoomLevel = 12;
+            MapControl1.LandmarksVisible = true;
+        }
+        private void SpaceNeedle_Click(object sender, RoutedEventArgs e)
+        {
+            Geopoint spaceNeedlePoint = new Geopoint
+                (new BasicGeoposition { Latitude = 47.6205, Longitude = -122.3493 });
+
+            PlaceInfoCreateOptions options = new PlaceInfoCreateOptions();
+
+            options.DisplayAddress = "400 Broad St, Seattle, WA 98109";
+            options.DisplayName = "Seattle Space Needle";
+
+            PlaceInfo spaceNeedlePlace = PlaceInfo.Create(spaceNeedlePoint, options);
+
+            FrameworkElement targetElement = (FrameworkElement)sender;
+
+            GeneralTransform generalTransform =
+                targetElement.TransformToVisual((FrameworkElement)targetElement.Parent);
+
+            Rect rectangle = generalTransform.TransformBounds(new Rect(new Point
+                (targetElement.Margin.Left, targetElement.Margin.Top), targetElement.RenderSize));
+
+            spaceNeedlePlace.Show(rectangle, Windows.UI.Popups.Placement.Below);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            Windows.Storage.Pickers.FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // Get the current location.
+                    Geolocator geolocator = new Geolocator();
+                    while (true)
+                    {
+                        Geoposition pos = await geolocator.GetGeopositionAsync();
+                        Geopoint myLocation = pos.Coordinate.Point;
+                        string accuracy = pos.Coordinate.Accuracy.ToString();
+                        string speed = pos.Coordinate.Speed.ToString();
+                        string stdata = pos.Coordinate.SatelliteData.ToString();
+                        string heading = pos.Coordinate.Heading.ToString();
+                        string positionSource = pos.Coordinate.PositionSource.ToString();
+                        string positionSourceTS = pos.Coordinate.PositionSourceTimestamp.ToString();
+                        string altitude = myLocation.Position.Altitude.ToString();
+                        string altitudeAccuracy = pos.Coordinate.AltitudeAccuracy.ToString();
+                        string longtitude = myLocation.Position.Longitude.ToString();
+                        string latitude = myLocation.Position.Latitude.ToString();
+                        // Set the map location.
+                        string time1 = pos.Coordinate.Timestamp.ToString();
+                        MapControl1.Center = myLocation;
+                        sb.AppendLine("Time Stamp: " + time1 +
+                            " Latitude: " + latitude +
+                            " Longtitude: " + longtitude +
+                            " Accuracy: " + accuracy);
+                          //  " Heading: " + heading+
+                          //  " Speed: "+ speed+
+                          //  " SatelliteData: "+stdata+
+                          //  " Point: "+myLocation.ToString()+
+                          //  " Position Source: "+ positionSource+
+                          //  " Position Source Time Stamp: "+positionSourceTS+
+                          //  " Altitude: "+altitude+
+                          //  " Altitude Accuracy: "+altitudeAccuracy);
+                        MapControl1.ZoomLevel = 20;
+                        MapControl1.LandmarksVisible = true;
+                        messageb.Text = sb.ToString();
+                        await Windows.Storage.FileIO.WriteTextAsync(file, sb.ToString());
+                    }
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    // Handle the case  if access to location is denied.
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    // Handle the case if  an unspecified error occurs.
+                    break;
+            }
+        }
+    }
+}
---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.WiFi;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using System.Net.NetworkInformation;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using System.Text;
+using Windows.Devices.Sensors;
+using Windows.Graphics.Display;
+using Windows.UI.Input.Spatial;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       
+        WiFiAdapter wifiAdapter;
+        List<string> ls = new List<string>();
+        StringBuilder sb = new StringBuilder();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            HostName host = new HostName("www.mail.ru");
+            var eps = await DatagramSocket.GetEndpointPairsAsync(host, "80");
+            if (eps.Count >= 1)
+            {
+                messageb.Text = eps.Count.ToString()+"\n";
+            }
+            else
+            {
+                messageb.Text="error\n";
+            }
+            //Ping ping = new Ping();
+            // Send a ping.
+            //PingReply reply = await ping.SendPingAsync("dotnetperls.com");
+            // Display the result.
+            //Text.Text = "ADDRESS:" + reply.Address.ToString();
+            //Text.Text = Text.Text + "TIME:" + reply.RoundtripTime;
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            Windows.Storage.Pickers.FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            sb.AppendLine("time;ssid1;strength1;ssid2;strength2;ssid3;strength3;ssid4;strength4;ssid5;strength5");
+            WiFiAccessStatus access = await WiFiAdapter.RequestAccessAsync();
+            DateTimeOffset time = DateTimeOffset.Now;
+            sb.AppendLine(time.ToString()+time.Millisecond);
+            if (access == WiFiAccessStatus.Allowed)
+            {
+                DataContext = this;
+                var result = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector());
+                //time = DateTimeOffset.Now;
+                //sb.AppendLine(time.ToString() + time.Millisecond);
+                if (result.Count >= 1)
+                {
+                    wifiAdapter = await WiFiAdapter.FromIdAsync(result[0].Id);
+                    //time = DateTimeOffset.Now;
+                    //sb.AppendLine(time.ToString() + time.Millisecond);
+                }
+            }
+            time = DateTimeOffset.Now;
+            //sb.AppendLine(time.ToString() + time.Millisecond);
+           var result1= await WiFiAdapter.FindAllAdaptersAsync();
+            sb.AppendLine(result1.GetType().ToString());
+           while(true)
+            {
+                var firstadapter = result1[0];
+                await firstadapter.ScanAsync();
+                 time= DateTimeOffset.Now;
+                
+                //await Task.Delay(5000);
+                sb.Append("  " +time.Hour+":"+time.Minute+":"+time.Second+":"+time.Millisecond + "   ;   ");
+                
+               // List<string> sortedList = new List<string>();
+                foreach (var network in firstadapter.NetworkReport.AvailableNetworks)
+                {
+                    //var _dist = Math.Pow(10, 0.05 * (-20 * Math.Log10(network.ChannelCenterFrequencyInKilohertz / 1000) + 27.55 + Math.Abs(network.NetworkRssiInDecibelMilliwatts)));
+                    //ls.Add(Math.Round(_dist, 1) + "m " + " Name : " + network.Ssid + " , dB signal RSSID : " + network.NetworkRssiInDecibelMilliwatts+network.Uptime.ToString(@"\:mm\:ss\:fff"));
+                    sb.Append( network.Ssid+"    ;    "+network.NetworkRssiInDecibelMilliwatts+" ;  "+ network.Bssid+" ;  "+network.ChannelCenterFrequencyInKilohertz+" ; ");
+                }
+
+                sb.AppendLine();
+                messageb.Text = sb.ToString();
+                //connectpanel.Visibility = Visibility.Collapsed;
+                await Windows.Storage.FileIO.WriteTextAsync(file, sb.ToString());
+                //sortedList = ls.OrderBy(s => double.Parse(s.Substring(0, s.IndexOf('m')))).ToList();
+               
+            }
+            
+
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.Pickers.FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+
+            for(int i=0;i<100;i++)
+            {                
+                DateTimeOffset time = DateTimeOffset.Now;
+                //await Task.Delay(5000);
+                sb.Append("Time:  " + time.Hour + ":" + time.Minute + ":" + time.Second + ":" + time.Millisecond + "   :   ");
+                
+
+                //sortedList = ls.OrderBy(s => double.Parse(s.Substring(0, s.IndexOf('m')))).ToList();
+
+                
+                //connectpanel.Visibility = Visibility.Collapsed;
+
+            }
+            //messageb.Text = sb.ToString();
+            messageb.Text = sb.ToString();
+            await Windows.Storage.FileIO.WriteTextAsync(file, sb.ToString());
+        }
+
+        private void ReadingChanged(object sender, GyrometerReadingChangedEventArgs e)
+        {
+            double x_Axis=0;
+            double y_Axis=0;
+            double z_Axis=0;
+
+            GyrometerReading reading = e.Reading;
+
+            // Calculate the gyrometer axes based on
+            // the current display orientation.
+            DisplayInformation displayInfo = DisplayInformation.GetForCurrentView();
+            switch (displayInfo.CurrentOrientation)
+            {
+                case DisplayOrientations.Landscape:
+                    x_Axis = reading.AngularVelocityX;
+                    y_Axis = reading.AngularVelocityY;
+                    z_Axis = reading.AngularVelocityZ;
+                    break;
+                case DisplayOrientations.Portrait:
+                    x_Axis = reading.AngularVelocityY;
+                    y_Axis = -1 * reading.AngularVelocityX;
+                    z_Axis = reading.AngularVelocityZ;
+                    break;
+                case DisplayOrientations.LandscapeFlipped:
+                    x_Axis = -1 * reading.AngularVelocityX;
+                    y_Axis = -1 * reading.AngularVelocityY;
+                    z_Axis = reading.AngularVelocityZ;
+                    break;
+                case DisplayOrientations.PortraitFlipped:
+                    x_Axis = -1 * reading.AngularVelocityY;
+                    y_Axis = reading.AngularVelocityX;
+                    z_Axis = reading.AngularVelocityZ;
+                    break;
+            }
+            sb.AppendLine(x_Axis + " ; " + y_Axis + " ; " + z_Axis);
+            messageb.Text = sb.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
+   
